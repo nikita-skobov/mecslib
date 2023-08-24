06208b3c20388d6e356575233afa041c33a56db9
nikita-skobov <nskobov.websites@gmail.com>
2023-08-24 17:25:53 -0500

    removes player character. map turns bg blue when done tiling

--- bin/game.rs
+++ bin/game.rs
@@ -22,7 +22,6 @@ pub struct FilledMap {
 
 
 pub struct MyState {
-    pub character: Entity,
     pub rand_map: RandomMapGen,
     pub rng: fastrand::Rng,
     pub filled: FilledMap,
@@ -36,7 +35,6 @@ impl Default for MyState {
     fn default() -> Self {
         Self {
             rng: fastrand::Rng::with_seed(7),
-            character: Entity::DANGLING,
             rand_map: Default::default(),
             filled: Default::default(),
             created_tile_map: Default::default(),
@@ -56,18 +54,18 @@ impl UserState<Textures> for MyState {
         let intensity = density / 2.0;
         s.usr.voronoi_tiling.with_grid_points(grid_size, density as _, intensity);
 
-        let transform = Transform::from_scale_angle_position(1.0, 0.0, (0.0, 0.0));
-        let draw = Drawable::texture(s, Textures::test);
-        let unit = s.world.spawn((transform, draw, Layer1));
+        // example of parenting:
+        // let transform = Transform::from_scale_angle_position(1.0, 0.0, (0.0, 0.0));
+        // let draw = Drawable::texture(s, Textures::test);
+        // let unit = s.world.spawn((transform, draw, Layer1));
 
-        let transform = Transform::from_scale_angle_position(1.0, std::f32::consts::FRAC_PI_4, (10.0, 0.0));
-        let draw = Drawable::texture(s, Textures::other);
-        let parent = Parent {
-            parent: unit,
-            local_transform: transform,
-        };
-        let _other = s.world.spawn((Transform::default(), draw, parent, Layer2));
-        s.usr.character = unit;
+        // let transform = Transform::from_scale_angle_position(1.0, std::f32::consts::FRAC_PI_4, (10.0, 0.0));
+        // let draw = Drawable::texture(s, Textures::other);
+        // let parent = Parent {
+        //     parent: unit,
+        //     local_transform: transform,
+        // };
+        // let _other = s.world.spawn((Transform::default(), draw, parent, Layer2));
     }
 }
 
@@ -84,7 +82,6 @@ const HOVERED_TILE_COLOR: Color = WHITE;
 fn get_all_systems() -> &'static [MySystem] {
     &[
         sys!(handle_pan),
-        sys!(control_character),
         sys!(update_children_transforms),
         sys!(generate_tiles_voronoi),
         sys!(draw_hovered_tiles),
@@ -252,6 +249,8 @@ fn generate_tiles_voronoi(s: &mut GameState, _dt: f32) {
             // scale the number of points down proportionally to what was filled.
             // tiling.next_n(&mut s.usr.rng, 2);
             tiling.continue_with_open_set();
+        } else {
+            s.clear_color = WATER_COLOR;
         }
     }
 }
@@ -476,51 +475,6 @@ fn fill_generated_map(s: &mut GameState, _dt: f32) {
         let tint = Tint { d: color };
         s.world.spawn((transform, Layer0, tint, Drawable::Texture { d, dont_center: false }, BuildingMapTile));
     }
-}
-
-fn control_character(s: &mut GameState, _dt: f32) {
-    let d_rotation = 0.01;
-    let d_move = 2.0;
-    let d_scale = 0.03;
-
-    let mut base_angle = 0.0;
-    let mut base_pos = Vec2::ZERO;
-    let mut base_scale = 1.0;
-    if is_key_down(KeyCode::Q) {
-        base_angle -= d_rotation;
-    }
-    if is_key_down(KeyCode::E) {
-        base_angle += d_rotation;
-    }
-    if is_key_down(KeyCode::W) {
-        base_pos.y -= d_move;
-    }
-    if is_key_down(KeyCode::S) {
-        base_pos.y += d_move;
-    }
-    if is_key_down(KeyCode::A) {
-        base_pos.x -= d_move;
-    }
-    if is_key_down(KeyCode::D) {
-        base_pos.x += d_move;
-    }
-    if is_key_down(KeyCode::Equal) {
-        base_scale += d_scale;
-    }
-    if is_key_down(KeyCode::Minus) {
-        base_scale -= d_scale;
-    }
-    if base_angle == 0.0 && base_pos == Vec2::ZERO && base_scale == 0.0 {
-        return;
-    }
-
-    let transform = match s.world.query_one_mut::<&mut Transform>(s.usr.character) {
-        Ok(x) => x,
-        Err(_) => return,
-    };
-    let movement = Transform::from_scale_angle_position(base_scale, base_angle, base_pos);
-    transform.d.matrix2 = transform.d.matrix2 * movement.d.matrix2;
-    transform.d.translation += base_pos;
 }
 
 #[macroquad::main("battlegame")]
